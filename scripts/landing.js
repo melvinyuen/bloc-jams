1b3358371ebd7b54d5743f30cd9a08948d2d76c5
Melvin Yuen <melvinyuen@gmail.com>
2017-02-03 06:14:52 -0800

    Added scrolling event handler to landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function() {               
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {               
+    
     var revealPoint = function(index) {
      
          points[index].style.opacity = 1;
@@ -14,3 +15,18 @@ var animatePoints = function() {
     }
  };
 
+window.onload = function() {
+    // Automagically animate the points on a tall screens where scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+        animatePointws(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+}
