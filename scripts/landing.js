2d31b5acb1742384d6db864a7a155fdcfeb73909
Melvin Yuen <melvinyuen@gmail.com>
2017-02-03 08:24:29 -0800

    deleted space on line 3 after function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function (point){   
+var revealPoint = function(point){   
      point.style.opacity = 1;
      point.style.transform = "scaleX(1) translatey(0)";
      point.style.msTransform = "scaleX(1) translateY(0)";
