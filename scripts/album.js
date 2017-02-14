f3ec06c005a064ab01ba98d8b8e4a90e01ac34e6
Melvin Yuen <melvinyuen@gmail.com>
2017-02-14 14:03:03 -0800

    fixed typo in line 7 'covers' q

--- scripts/album.js
+++ scripts/album.js
@@ -4,7 +4,7 @@ var albumPicasso = {
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
-    albumArtUrl: 'assets/images/album_cover/01.png',
+    albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
         { title: 'Blue', duration: '4:26'},
         { title: 'Green', duration: '3:14'},
@@ -60,7 +60,7 @@ var setCurrentAlbum = function(album) {
     albumSongList.innerHTML = '';
     
     // #4
-    for (var i = o; i < album.songs.length; i++) {
+    for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
