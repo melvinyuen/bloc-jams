2a15c9d412fa3d379e9ef362b8d4a111e16c3562
Melvin Yuen <melvinyuen@gmail.com>
2017-02-14 13:09:23 -0800

    added album.js from album scrolling when clicking image

--- scripts/album.js
+++ scripts/album.js
@@ -4,7 +4,7 @@ var albumPicasso = {
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
-    albumArtUrl: 'assets/images/album_cover/01.png',
+    albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
         { title: 'Blue', duration: '4:26'},
         { title: 'Green', duration: '3:14'},
@@ -30,6 +30,23 @@ var albumMarconi = {
     ]
 };
 
+// Third Example Album object Assignment 11
+var albumMelvin = {
+    title: 'Melvin First Album',
+    artist: 'Melvin Yuen',
+    label: 'Melvin',
+    year: '1982',
+    albumArtUrl: 'assets/images/album_covers/19.png',
+    songs: [
+        { title: 'Melvin, Song 1', duration: '1:01'},
+        { title: 'Melvin, Song 2', duration: '2:02',},
+        { title: 'Melvin, Song 3', duration: '3:03',},
+        { title: 'Melvin, Song 4', duration: '4:04'},
+        { title: 'Melvin, Song 5', duration: '5:05'}
+    ]
+};
+
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class="album-view-song-item">'
@@ -42,13 +59,15 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
-var setCurrentAlbum = funtion(album) {
     // #1
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];     // set a var to the album cover element  
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    
+
+var setCurrentAlbum = function(album) {
     
     // #2
     albumTitle.firstChild.nodeValue = album.title;
@@ -60,11 +79,23 @@ var setCurrentAlbum = funtion(album) {
     albumSongList.innerHTML = '';
     
     // #4
-    for (var i = o; i < album.songs.length; i++) {
+    for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
 
-windows.onload = function() { 
+window.onload = function() { 
     setCurrentAlbum(albumPicasso);
-};
+    
+    var albums = [albumPicasso, albumMarconi, albumMelvin];                 // create an array of album objects
+    var index = 1;                                                          /* create a `index` var to track the array index to click: if index === 1, then we'll load index + 1 (or 2) */
+                                                                            //element.addEventListener("click", function(){ alert("Hello World!"); });
+    albumImage.addEventListener('click',function(event) {                   // create click event listener
+        setCurrentAlbum(albums[index]);                                     // inside the listener call setCurrentAlbum with the next album
+        index++;
+        if (index == albums.length) {                                        // need logic that if the last one they clicked was #2, show #0 [album1, album2, album3]
+            index = 0;                                                      
+        }
+     
+    });
+}
