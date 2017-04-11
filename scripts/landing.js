37b00ab9985275615a29d1821d925175f44e53c8
Melvin Yuen <melvinyuen@gmail.com>
2017-04-11 11:17:52 -0700

    refactored landing.js to eliminate repetitive code for D.R.Y.

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,15 +2,15 @@ var animatePoints = function() {
 
     var points = document.getElementsByClassName('point');
     
-    var revealPoint = function() {
+    var revealPoint = function(index) {    
         
-        for(var i = 0; i < points.length; i++){
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";                
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";                
+    }
         
-        revealPoint();
-        }
+    for(var i = 0; i < points.length; i++){
+        revealPoint(i);
     }
 };
