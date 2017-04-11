9c464980ce1d70be755c0de709bf902cc68cef2f
Melvin Yuen <melvinyuen@gmail.com>
2017-04-11 10:22:09 -0700

    fixed syntax error in landing.js, added

--- scripts/landing.js
+++ scripts/landing.js
@@ -5,11 +5,12 @@ var animatePoints = function() {
     var revealPoint = function() {
         
         for(var i = 0; i < points.length; i++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";                
+            points[i].style.opacity = 1;
+            points[i].style.transform = "scaleX(1) translateY(0)";
+            points[i].style.msTransform = "scaleX(1) translateY(0)";
+            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";                
         
         revealPoint();
+        }
     }
 };
